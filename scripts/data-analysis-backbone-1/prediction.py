6d622160df59485c1f31009dbd95804fadb9a6e9
Akos Schneider <61552438+akossch0@users.noreply.github.com>
2024-01-01 16:00:37 +0100

    lint

--- scripts/data-analysis-backbone-1/prediction.py
+++ scripts/data-analysis-backbone-1/prediction.py
@@ -7,28 +7,31 @@ import time
 
 def get_predictions(model, test_df: pd.DataFrame, trace_df: pd.DataFrame):
     pred_df = model.predict(test_df[trace_df.predictors[0]])
-    pred_df['prediction'] = list(pred_df.idxmax(axis=1)+1)
+    pred_df["prediction"] = list(pred_df.idxmax(axis=1) + 1)
     return pred_df
 
+
 print("Loading model training data from tracing db for model reconstruction.")
-con = duckdb.connect('datasets/trace/data-governance.db')
+con = duckdb.connect("datasets/trace/data-governance.db")
 trace_df = con.execute("select * from model_training").df()
 print("Model training data loaded.")
 con.close()
 
 print("Loading model from pickle file.")
-model_path = 'datasets/predict/model/model.pkl'
-with open(model_path, 'rb') as f:
-  model = pickle.load(f)
+model_path = "datasets/predict/model/model.pkl"
+with open(model_path, "rb") as f:
+    model = pickle.load(f)
 print("Model loaded.")
 
 
-input_dir = 'datasets/predict/input'
+input_dir = "datasets/predict/input"
 files = os.listdir(input_dir)
-csv_files = [f for f in files if f.endswith('.csv')]
+csv_files = [f for f in files if f.endswith(".csv")]
 
 if csv_files:
-    latest_file = max(csv_files, key=lambda f: os.path.getmtime(os.path.join(input_dir, f)))
+    latest_file = max(
+        csv_files, key=lambda f: os.path.getmtime(os.path.join(input_dir, f))
+    )
     file_path = os.path.join(input_dir, latest_file)
     df = pd.read_csv(file_path)
 else:
@@ -37,12 +40,12 @@ else:
 print("Running predictions on {}.".format(file_path))
 pred_df = get_predictions(model, df, trace_df)
 
-output_dir = 'datasets/predict/output'
+output_dir = "datasets/predict/output"
 if not os.path.exists(output_dir):
     os.makedirs(output_dir)
 
 timestamp_millis = str(int(time.time() * 1000))
-output_filename = latest_file[:-4] + '_predictions_' + str(timestamp_millis) + '.csv'
+output_filename = latest_file[:-4] + "_predictions_" + str(timestamp_millis) + ".csv"
 output_path = os.path.join(output_dir, output_filename)
 pred_df.to_csv(output_path, index=False)
-print("Predictions saved to {}.".format(file_path[:-4] + '_predictions.csv'))
+print("Predictions saved to {}.".format(file_path[:-4] + "_predictions.csv"))
